fc01ede6ce646239559aaaaafbbf30c3f54b69e2
Benjamin Malley <benjamin.malley@gmail.com>
2011-11-28 14:01:31 -0500

    added a setInterval timer to slideshow.js

--- showcase.js
+++ showcase.js
@@ -1,7 +1,21 @@
 var showcase_width;
+var animation_time = 2000;
+
+var next_slide = function() {
+	$('#slide-reel').animate({left: '-=' + showcase_width, }, animation_time, function() {   //move the slide-reel to the left
+		$('#slide-reel').css('padding-left', '+=' + showcase_width);   //pad the left of the slide-reel
+		$('#slide-reel').append($('.slide-wrapper:first'));   //move the first slide to the end of the reel
+	});
+};
+
+var previous_slide = function() { 
+	$('#slide-reel').css('padding-left', '-=' + showcase_width);   //make some room on the left side of the slide-reel for an additional slide
+	$('#slide-reel').prepend($('.slide-wrapper:last'));   //move the last slide to the beginning of the reel
+	$('#slide-reel').animate({'padding-left': '+=' + showcase_width, }, animation_time);   //move the slide-reel to the right
+};
 
 $(document).ready(function() {
-	var animation_time = 2000;
+
 	showcase_width = $('#showcase').css('width');   //set the initial showcase width
 
 	$('#slide-reel').css({
@@ -12,22 +26,13 @@ $(document).ready(function() {
 	});
 	$('.slide-wrapper').css('width', showcase_width);   //make the slide-wrappers the same width as the showcase
 	
-
-	$(document).on('click', '.slide-button.right', function() {   //handle a click to move forward one slide
-		$('#slide-reel').animate({left: '-=' + showcase_width, }, animation_time, function() {   //move the slide-reel to the left
-			$('#slide-reel').css('padding-left', '+=' + showcase_width);   //pad the left of the slide-reel
-			$('#slide-reel').append($('.slide-wrapper:first'));   //move the first slide to the end of the reel
-		});
-	});
+	$(document).on('click', '.slide-button.right', next_slide);
 	
+	$(document).on('click', '.slide-button.left', previous_slide); 
 
-	$(document).on('click', '.slide-button.left', function() {   //handle a click to move backward one slide
-		$('#slide-reel').css('padding-left', '-=' + showcase_width);   //make some room on the left side of the slide-reel for an additional slide
-		$('#slide-reel').prepend($('.slide-wrapper:last'));   //move the last slide to the beginning of the reel
-		$('#slide-reel').animate({'padding-left': '+=' + showcase_width, }, animation_time);   //move the slide-reel to the right
-	});
+	setInterval(next_slide, 10000);
 });
-
+	
 //resize the slideshow
 $(window).resize(function() {
 	showcase_width = $('#showcase').css('width');	
